a6c0be9904dca3f05e0d7dea7813a937b6c9bf5d
Justin Luoma <justin@justinluoma.com>
2018-09-08 15:30:13 -0500

    Header refactored except scroll button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import './index.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-injectTapEventPlugin();
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
