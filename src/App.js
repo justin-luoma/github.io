3a2e3845ea41a35f2f83bc41b90ec976f9ab8a50
Justin Luoma <justin@justinluoma.com>
2018-09-08 19:40:41 -0500

    Finished header and intro components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
-import HeaderComponent from './components/Header/Header'
+import PropTypes from 'prop-types'
+import { withStyles } from "@material-ui/core/styles";
+import HeaderComponent from './components/Header/Header';
+import IntroductionComponent from './components/Introduction/Introduction';
+import TimelineComponent from './components/Timeline/Timeline';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
+
+const styles = {
+
+};
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
-        <div className="section header-section">
-            <div className="section-container header-component">
-              <HeaderComponent></HeaderComponent>
+      <React.Fragment>
+        <CssBaseline />
+        <div className="App">
+          <div className="section header-section">
+              <div className="section-container header-component">
+                <HeaderComponent />
+              </div>
             </div>
-          </div>
-      </div>
+            <div className="section colored">
+              <div className="section-container">
+                <IntroductionComponent />
+              </div>
+            </div>
+            <div className="section">
+              <div className="section-container">
+                <TimelineComponent />
+              </div>
+            </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(App);
